88f2b84987dfc9d1947910e41939ce7034f0789e
Rasim Eminov <eminov.workspace@yandex.ru>
2024-06-08 02:29:19 +0300

    feat(marshalling): add ISerializer

---
+++ src/site/buraki/observer/infrastructure/marshalling/ISerializer.java
@@ -0,0 +1,18 @@
+package site.buraki.observer.infrastructure.marshalling;
+
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Абстрактный сериализатор.
+ *
+ * @author Расим "Buraki" Эминов
+ * @param <T> Тип сериализуемого объекта.
+ * @since 0.2.0
+ */
+public interface ISerializer<T> {
+
+    void serializeOne(T object, OutputStream outputStream);
+
+    void serializeMany(List<T> objects, OutputStream outputStream);
+}
