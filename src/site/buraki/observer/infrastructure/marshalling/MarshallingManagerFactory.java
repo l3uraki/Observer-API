e81c329cbc381f2cace75fbc1d21222da2220ac7
Rasim Eminov <eminov.workspace@yandex.ru>
2024-06-08 02:41:56 +0300

    feat(marshalling): add MarshallingManagerFactory

---
+++ src/site/buraki/observer/infrastructure/marshalling/MarshallingManagerFactory.java
@@ -0,0 +1,11 @@
+package site.buraki.observer.infrastructure.marshalling;
+
+/**
+ * Фабрика менеджеров маршалинга.
+ *
+ * @author Расим "Buraki" Эминов
+ * @see IMarshallingManager
+ * @since 0.2.0
+ */
+public class MarshallingManagerFactory {
+}
